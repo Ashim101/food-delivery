c7924059eb1f8d549bdadd10b751450dc733ebbb
ashim101 <ashimsapkota250@gmail.com>
2025-01-06 23:32:10 +0545

    initial

---
+++ backend/src/index.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => { console.log("database connected") })
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+app.get("/test", async (req: Request, res: Response) => {
+    res.json({ msg: "Welcome to the app" })
+});
+
+app.listen(7000, () => {
+    console.log("listening on the port 7000")
+});
