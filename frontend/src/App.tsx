ee381a6d9accdd7807758082c41f5d5277d19461
ashim101 <ashimsapkota250@gmail.com>
2025-01-07 00:11:53 +0545

    routes

--- frontend/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@/components/ui/button"
-
-export default function Home() {
-  return (
-    <div className="flex min-w-full min-h-full justify-center items-center">
-      <Button>Click me</Button>
-    </div>
-  )
-}
