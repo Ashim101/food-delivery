235d4815d5ff9ee43bf48a781ee0cdd6b549dca6
ashim101 <ashimsapkota250@gmail.com>
2025-01-18 22:37:38 +0545

    card

--- frontend/src/components/SearchResultCard.tsx
+++ frontend/src/components/SearchResultCard.tsx
@@ -1,6 +1,13 @@
 import { Restraurant } from "@/types";
 import { AspectRatio } from "@radix-ui/react-aspect-ratio";
-import { Banknote, Clock, Dot, Watch, WatchIcon } from "lucide-react";
+import {
+  Banknote,
+  Clock,
+  ClockAlert,
+  Dot,
+  Watch,
+  WatchIcon,
+} from "lucide-react";
 import { Link } from "react-router-dom";
 
 type Props = {
@@ -9,40 +16,50 @@ type Props = {
 
 const SearchResultCard = ({ restraurant }: Props) => {
   return (
+    <Link to={`/details/${restraurant._id}`}>
+      <div className="grid lg:grid-cols-[2fr,3fr] gap-4 group">
+        <AspectRatio ratio={16 / 9}>
+          <img
+            src={restraurant.imageUrl}
+            alt=""
+            className="w-full h-full object-cover rounded-md "
+          />
+        </AspectRatio>
 
-      
-      <Link to={`/details/${restraurant._id}`}>
-        <div className="grid lg:grid-cols-[2fr,3fr] gap-2 group">
-            <AspectRatio ratio={16/9}>
-                <img src={restraurant.imageUrl} alt="" className="w-full h-full object-cover rounded-md" />
-            </AspectRatio>
-
-            <div className="flex gap-4">
-                <div className="flex flex-col gap-5 w-full">
-                    <p className="text-2xl font-bold">{restraurant.restraurantName}</p>
-
-                    <div className="flex flex-wrap gap-3">
-                        {restraurant.cuisines.map((cuisine)=>(<p className="flex gap-1">{cuisine}<Dot/></p>))}
-                    </div>
-
-                </div>
-                <div className="w-full flex flex-col justify-center gap-2">
-                    <p className="flex gap-2 text-green-700"><Clock/>{restraurant.delieveryPrice} mins</p>
-                    <p className="flex gap-2 "> <Banknote/> Delievery from {restraurant.estimatedDelieveryTime}</p>
-
-
-                </div>
-
+        <div className="flex flex-col  w-full gap-4">
+          <p className="text-2xl font-bold group-hover:underline">
+            {restraurant.restraurantName}
+          </p>
+          <div className="flex flex-col lg:flex-row gap-5 justify-between lg:pr-2 lg:gap-4 ">
+            <div className="flex  flex-wrap  gap-2  ">
+              {restraurant.cuisines.map((cuisine, index) => (
+                <p
+                  key={index}
+                  className="flex items-center text-sm leading-none "
+                >
+                  {cuisine}
+                  {index !== restraurant.cuisines.length - 1 && (
+                    <Dot className="ml-1" />
+                  )}
+                </p>
+              ))}
             </div>
 
-            <div >
-
+            <div className="space-y-2">
+              <div className="flex gap-1">
+                <Clock className="text-green-600" />
+                <p>{restraurant.estimatedDelieveryTime} mins</p>
+              </div>
+              <div className="flex gap-1">
+                <Banknote />
+                <p>{restraurant.delieveryPrice}</p>
+              </div>
             </div>
-
+          </div>
         </div>
-      
-      </Link>
-    )
+      </div>
+    </Link>
+  );
 };
 
 export default SearchResultCard;
